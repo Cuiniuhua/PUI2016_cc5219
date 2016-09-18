ce7292c17df1fb1c7a038d49a6d800d5c42b6743
Chenxi Cui <cc5219@compute.cusp.nyu.edu>
2016-09-18 13:27:10 -0400

    2s

--- HW2_cc5219/show_bus_locations_cc5219.py
+++ HW2_cc5219/show_bus_locations_cc5219.py
@@ -5,17 +5,17 @@ import json
 import urllib.request as ulr
 import os
 import sys
+import numpy as np
 key=sys.argv[1]
 busline=sys.argv[2]
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + "&VehicleMonitoringDetailLevel=calls&LineRef="+ busline
 requrl = requests.get(url)
 data=requrl.json()
 buses= data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
-activebus=0
+numbus=np.size(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
 print ("Bus Line: %s"%(busline))
+print ("Number of Active Buses: %d" %(activebus))
 for i in range(len(buses)):
-     if buses[i]['MonitoredVehicleJourney']['PublishedLineName'] == busline:
-        activebus += 1
         lattd = buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
         lontd = buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-        print("Bus %s is at latitude %s and longtitude %s" %(activebus,lattd,lontd))
+        print("Bus %s is at latitude %s and longtitude %s" %(i+1,lattd,lontd))
