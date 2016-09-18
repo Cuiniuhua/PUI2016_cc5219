79ad54f4f104ad48b5f7fe739d64bc55fee4a5cd
Cuiniuhua <cc5219@nyu.edu>
2016-09-18 13:57:20 -0400

    Update show_bus_locations_cc5219.py

--- HW2_cc5219/show_bus_locations_cc5219.py
+++ HW2_cc5219/show_bus_locations_cc5219.py
@@ -7,13 +7,13 @@ import os
 import sys
 import numpy as np
 key=sys.argv[1]
-busline=sys.argv[2]
+bus_line=sys.argv[2]
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + "&VehicleMonitoringDetailLevel=calls&LineRef="+ busline
 requrl = requests.get(url)
 data=requrl.json()
 buses= data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 numbus=np.size(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
-print ("Bus Line: %s"%(busline))
+print ("Bus Line: %s"%(bus_line))
 print ("Number of Active Buses: %d" %(activebus))
 for i in range(len(buses)):
         lattd = buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
