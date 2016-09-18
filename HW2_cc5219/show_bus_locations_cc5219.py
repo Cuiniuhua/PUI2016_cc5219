1c70dddfa84fb6f6718cad83b55320f2fba6a805
Chenxi Cui <cc5219@compute.cusp.nyu.edu>
2016-09-18 14:33:09 -0400

    3

--- HW2_cc5219/show_bus_locations_cc5219.py
+++ HW2_cc5219/show_bus_locations_cc5219.py
@@ -2,7 +2,6 @@
 from __future__ import print_function
 import requests
 import json
-import urllib.request as ulr
 import os
 import sys
 import numpy as np
@@ -14,8 +13,9 @@ data=requrl.json()
 buses= data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 numbus=np.size(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
 print ("Bus Line: %s"%(busline))
-print ("Number of Active Buses: %d" %(activebus))
+print ("Number of Active Buses: %d" %(numbus))
 for i in range(len(buses)):
         lattd = buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
         lontd = buses[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
         print("Bus %s is at latitude %s and longtitude %s" %(i+1,lattd,lontd))
+
